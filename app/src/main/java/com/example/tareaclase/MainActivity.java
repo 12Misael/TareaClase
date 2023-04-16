28cb3494c21ee398a19b2e886ef831291d9771b4
Misael Cortes <josuemisael911@gmail.com>
2023-04-15 20:27:34 -0600

    commit

--- app/src/main/java/com/example/tareaclase/MainActivity.java
+++ app/src/main/java/com/example/tareaclase/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.tareaclase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        val REQUEST_VIDEO_CAPTURE = 1
+        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also {
+            Intent takeVideoIntent;
+            PackageManager packageManager;
+            takeVideoIntent -
+                takeVideoIntent.resolveActivity(packageManager)?.also {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+        }
+        }
     }
+
+
+
+
 }
